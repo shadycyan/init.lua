03b9867193da88d1b1088a3ba0e993016d81f1f6
Cyan <77025112+shadycyan@users.noreply.github.com>
2023-11-16 16:35:16 +0330

    update lsp-zero to v3.x

--- lua/shadycyan/plugins/lsp.lua
+++ lua/shadycyan/plugins/lsp.lua
@@ -1,46 +1,45 @@
 return {
-	"VonHeikemen/lsp-zero.nvim",
-	branch = "v2.x",
+	'VonHeikemen/lsp-zero.nvim',
+	branch = 'v3.x',
 	dependencies = {
-		-- LSP Support
-		{ "neovim/nvim-lspconfig" },
 		{
-			"williamboman/mason.nvim",
-			build = function()
-				pcall(vim.cmd, "MasonUpdate")
-			end,
+			-- LSP Support
+			{ 'neovim/nvim-lspconfig' },
+			{ 'williamboman/mason.nvim' },
+			{ 'williamboman/mason-lspconfig.nvim' },
+			-- Autocompletion
+			{ 'hrsh7th/cmp-nvim-lsp' },
+			{ 'hrsh7th/nvim-cmp' },
+			{ 'L3MON4D3/LuaSnip' },
+
+			{ 'akinsho/flutter-tools.nvim' },
+
+			{ 'nvim-lua/plenary.nvim' },
+			{ 'stevearc/dressing.nvim' },
 		},
-		{ "williamboman/mason-lspconfig.nvim" },
-
-		-- Autocompletion
-		{ "hrsh7th/nvim-cmp" },
-		{ "hrsh7th/cmp-buffer" },
-		{ "hrsh7th/cmp-path" },
-		{ "saadparwaiz1/cmp_luasnip" },
-		{ "hrsh7th/cmp-nvim-lsp" },
-		{ "hrsh7th/cmp-nvim-lua" },
-
-		-- Snippets
-		{ "L3MON4D3/LuaSnip" },
-		{ "rafamadriz/friendly-snippets" },
-
-		-- Language support, mainly for indentation
-		{ "dart-lang/dart-vim-plugin" },
-
-		{ "j-hui/fidget.nvim", tag = "legacy" },
 	},
 	config = function()
-		local lsp_zero = require("lsp-zero")
-		local lsp_config = require("lspconfig")
+		local lsp_zero = require('lsp-zero')
 
+		-- -- (Optional) configure lua language server
+		-- local lua_opts = lsp_zero.nvim_lua_ls()
+		-- require('lspconfig').lua_ls.setup(lua_opts)
 
-		lsp_zero.preset("minimal")
-
-		lsp_zero.ensure_installed({
-			"lua_ls",
-			"yamlls",
-			"jsonls",
+		-- Replace the language servers listed here
+		-- with the ones you have installed
+		lsp_zero.setup_servers({
+			'dartls',
 		})
+
+		-- lsp_zero.ensure_installed({
+		-- 	"lua_ls",
+		-- 	"yamlls",
+		-- 	"jsonls",
+		-- })
+
+		require 'lspconfig'.dartls.setup {
+			cmd = { "dart", 'language-server', '--protocol=lsp' },
+		}
 
 		-- Fix Undefined global 'vim'
 		lsp_zero.configure("lua_ls", {
@@ -53,109 +52,61 @@ return {
 			},
 		})
 
-		local on_attach = function(_, bufnr)
-			local opts = { buffer = bufnr }
-
-			vim.keymap.set("n", "gd", function()
-				vim.lsp.buf.definition()
-			end, opts)
-			vim.keymap.set("n", "K", function()
-				vim.lsp.buf.hover()
-			end, opts)
-			vim.keymap.set("n", "<leader>e", function()
-				vim.diagnostic.open_float()
-			end, opts)
-			vim.keymap.set("n", "[d", function()
-				vim.diagnostic.goto_prev()
-			end, opts)
-			vim.keymap.set("n", "]d", function()
-				vim.diagnostic.goto_next()
-			end, opts)
-			vim.keymap.set("n", "<leader>ca", function()
-				vim.lsp.buf.code_action()
-			end, opts)
-			vim.keymap.set("n", "<leader>gr", function()
-				vim.lsp.buf.references()
-			end, opts)
-			vim.keymap.set("n", "<leader>rn", function()
-				vim.lsp.buf.rename()
-			end, opts)
-			vim.keymap.set({ "n", "i" }, "<C-b>", function()
-				vim.lsp.buf.inlay_hint(0, nil)
-			end, opts)
-			vim.keymap.set("i", "<C-h>", function()
-				vim.lsp.buf.signature_help()
-			end, opts)
-			vim.keymap.set("n", "<leader>ds", function()
-				vim.diagnostic.setqflist()
-			end, opts)
-		end
-
-		lsp_zero.on_attach(on_attach)
-
-		vim.diagnostic.config({
-			virtual_text = true,
-			signs = false,
-		})
-
-		lsp_config["dartls"].setup({
-			on_attach = on_attach,
-			settings = {
-				dart = {
-					analysisExcludedFolders = {
-						vim.fn.expand("$HOME/AppData/Local/Pub/Cache"),
-						vim.fn.expand("$HOME/.pub-cache"),
-						vim.fn.expand("/opt/homebrew/"),
-						vim.fn.expand("$HOME/tools/flutter/"),
-					},
-					updateImportsOnRename = true,
-					completeFunctionCalls = true,
-					showTodos = true,
-				},
+		require('mason').setup({})
+		require('mason-lspconfig').setup({
+			ensure_installed = {
+				"yamlls",
+				"dartls",
+				"jsonls",
 			},
+			handlers = {
+				lsp_zero.default_setup,
+				lua_ls = function()
+					local lua_opts = lsp_zero.nvim_lua_ls()
+					require('lspconfig').lua_ls.setup(lua_opts)
+				end,
+			}
 		})
 
-		lsp_config["lua_ls"].setup({
-      on_attach = on_attach,
-      settings = { -- custom settings for lua
-        Lua = {
-          -- make the language server recognize "vim" global
-          diagnostics = {
-            globals = { "vim" },
-          },
-          workspace = {
-            -- make language server aware of runtime files
-            library = {
-              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-              [vim.fn.stdpath("config") .. "/lua"] = true,
-            },
-          },
-        },
-      },
-    })
+		lsp_zero.on_attach(function(client, bufnr)
+			lsp_zero.default_keymaps({ buffer = bufnr })
+		end)
 
-		lsp_zero.setup()
-
-		local cmp = require("cmp")
-
-		local cmp_mappings = cmp.mapping.preset.insert({
-			["<C-b>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-y>"] = cmp.mapping.complete(),
-			["<C-e>"] = cmp.mapping.abort(),
-			["<CR>"] = cmp.mapping.confirm({ select = false }),
-		})
-
-		cmp.setup({
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "path" },
-				{ name = "luasnip" },
-				{ name = "buffer", keyword_length = 5 },
+		lsp_zero.format_on_save({
+			format_opts = {
+				async = false,
+				timeout_ms = 10000,
 			},
-			mapping = cmp_mappings,
+			servers = {
+				['tsserver'] = { 'javascript', 'typescript' },
+				['rust_analyzer'] = { 'rust' },
+			}
 		})
 
-		require("fidget").setup({})
-	end
+		local auto_group = vim.api.nvim_create_augroup("LspAuGroup", { clear = true })
+
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(args)
+				local client = vim.lsp.get_client_by_id(args.data.client_id)
+				-- highlight references
+				-- if client.server_capabilities.documentHighlightProvider then
+				-- 	vim.api.nvim_create_autocmd("CursorHold", {
+				-- 		callback = function() vim.lsp.buf.document_highlight() end,
+				-- 		group = auto_group,
+				-- 	})
+				-- 	vim.api.nvim_create_autocmd("CursorMoved", {
+				-- 		callback = function() vim.lsp.buf.clear_references() end,
+				-- 		group = auto_group,
+				-- 	})
+				-- end
+				-- formatting
+				if client.server_capabilities.documentFormattingProvider then
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						callback = function() vim.lsp.buf.format() end,
+						group = auto_group,
+					})
+				end
+			end,
+		})
+	end,
 }
